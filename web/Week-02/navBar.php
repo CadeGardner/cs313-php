e963f78a381f219f00529f21d8a5358ccaecbf12
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-14 22:07:19 -0700

    changes to try and make nav horizontal

--- web/Week-02/navBar.php
+++ web/Week-02/navBar.php
@@ -1,11 +1,12 @@
 <?php
-    echo "
+    echo "";
     <header>
+    <div class='headerContainer'>
       <nav>
-        <a href='home.php'>Home</a>
-        <a href='login.php'>Login</a>
-        <a href='about-us.php'>About Us</a>
+      <div> <a href='home.php'>Home</a> </div>
+      <div> <a href='login.php'>Login</a> </div>
+      <div> <a href='about-us.php'>About Us</a> </div>
       </nav>
     </header>
-    ";
+    #";
  ?>
