d577dcded1bbda6437651bafbbecd0aaafb257ee
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-14 22:37:53 -0700

    More tweaking to the nav bar

--- web/Week-02/navBar.php
+++ web/Week-02/navBar.php
@@ -3,9 +3,9 @@
     <header>
     <div class='headerContainer'>
 
-        <div> <li> <a href='home.php'>Home</a> </li> </div>
-        <div> <li> <a href='login.php'>Login</a> </li> </div>
-        <div> <li> <a href='about-us.php'>About Us</a> </li> </div>
+        <div class='linkContainer'> <li> <a href='home.php'>Home</a> </li> </div>
+        <div class='linkContainer'> <li> <a href='login.php'>Login</a> </li> </div>
+        <div class='linkContainer'> <li> <a href='about-us.php'>About Us</a> </li> </div>
 
     </div>
     </header>
