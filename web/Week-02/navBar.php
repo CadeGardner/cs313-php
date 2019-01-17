ce0f966d7dfb21f971cdc068826f61cd554550e4
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-17 10:39:41 -0700

    welcome back li

--- web/Week-02/navBar.php
+++ web/Week-02/navBar.php
@@ -4,9 +4,9 @@
       <h1>Planners 'R' Us</h1>
     </div>
     <div class='headerContainer'>
-        <div class='linkContainer'> <a href='home.php'>Home</a> </div>
-        <div class='linkContainer'> <a href='login.php'>Login</a> </div>
-        <div class='linkContainer'> <a href='about-us.php'>About Us</a> </div>
+        <div class='linkContainer'><li><a href='home.php'>Home</a></li></div>
+        <div class='linkContainer'><li><a href='login.php'>Login</a></li></div>
+        <div class='linkContainer'><li><a href='about-us.php'>About Us</a></li></div>
 
     </div>
     </header>
