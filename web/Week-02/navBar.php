7fa0e07f6244407f68212c25e8223c1a8aa814d3
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-17 10:45:51 -0700

    Testing header styles

--- web/Week-02/navBar.php
+++ web/Week-02/navBar.php
@@ -8,5 +8,10 @@
         <div class='linkContainer'><li><a href='login.php'>Login</a></li></div>
         <div class='linkContainer'><li><a href='about-us.php'>About Us</a></li></div>
 
+        <ul>
+          <li><a href='home.php'>Home</a></li>
+          <li><a href='login.php'>Login</a></li>
+          <li><a href='about-us.php'>About Us</a></li>
+        </ul>
     </div>
     </header>
