bd3505dc5bed71911eef9f58b6516b71d959fcfe
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-17 10:25:27 -0700

    Company Header added

--- web/Week-02/navBar.php
+++ web/Week-02/navBar.php
@@ -1,7 +1,9 @@
 
     <header>
     <div class='headerContainer'>
-
+         <div>
+           <h1>Planners 'R' Us</h1>
+         </div>
         <div class='linkContainer'> <li> <a href='home.php'>Home</a> </li> </div>
         <div class='linkContainer'> <li> <a href='login.php'>Login</a> </li> </div>
         <div class='linkContainer'> <li> <a href='about-us.php'>About Us</a> </li> </div>
