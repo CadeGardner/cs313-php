1297bb717b12b39ada92d958744080b031ad1cc9
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-17 10:33:01 -0700

    bye li

--- web/Week-02/navBar.php
+++ web/Week-02/navBar.php
@@ -1,12 +1,12 @@
 
     <header>
+    <div>
+      <h1>Planners 'R' Us</h1>
+    </div>
     <div class='headerContainer'>
-         <div>
-           <h1>Planners 'R' Us</h1>
-         </div>
-        <div class='linkContainer'> <li> <a href='home.php'>Home</a> </li> </div>
-        <div class='linkContainer'> <li> <a href='login.php'>Login</a> </li> </div>
-        <div class='linkContainer'> <li> <a href='about-us.php'>About Us</a> </li> </div>
+        <div class='linkContainer'> <a href='home.php'>Home</a> </div>
+        <div class='linkContainer'> <a href='login.php'>Login</a> </div>
+        <div class='linkContainer'> <a href='about-us.php'>About Us</a> </div>
 
     </div>
     </header>
