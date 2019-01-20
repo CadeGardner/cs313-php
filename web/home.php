1d98be8de4bec4846c0203c5776974d7baba9881
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-19 23:08:08 -0700

    Forgot to save changes

---
+++ web/home.php
@@ -0,0 +1,50 @@
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1">
+     <title>Welcome Home</title>
+     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
+
+     <!-- jQuery library -->
+     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+     <!-- Popper JS -->
+     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
+
+     <!-- Latest compiled JavaScript -->
+     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
+
+   </head>
+   <body>
+     <div class="container">
+
+       <!-- Nav Bar -->
+       <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+         <ul class="navbar-nav">
+           <li class="nav-item active">
+             <a class="nav-link" href="#">Home</a>
+           </li>
+           <li class="nav-item">
+             <a class="nav-link" href="#">Assignments</a>
+         </ul>
+
+       </nav>
+
+       <div class="col-sm-5">
+         <img class="img-fluid" class="rounded" class="float-left" src="me.jpg" alt="">
+       </div>
+       <div class="col-sm-8">
+         <p>
+           My name is Cade and I really like these type of courses because you really <br>
+           get to see how things work under the hood. I like being able to see how websites <br>
+           work and design my own.
+
+           I also really enjoy Soccer. It amazes me what people can do with their feet and a <br>
+           soccer ball. A YouTube channel called <mark>"The F2"</mark> has really inspired me to <br>
+           get better at the <span class="font-italic">Beautiful</span> game.
+         </p>
+       </div>
+     </div>
+   </body>
+ </html>
