c943ad18aa59df52d47bf27f90da476fae81867c
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-20 00:03:19 -0700

    navbar test

--- web/navbar.php
+++ web/navbar.php
@@ -1,14 +1,14 @@
 <?php
 
-    echo "<nav class='navbar navbar-expand-sm bg-dark navbar-dark'>
-      <ul class='navbar-nav'>
-        <li class='nav-item active'>
-          <a class='nav-link' href='https://guarded-anchorage-18964.herokuapp.com/home.php'>Home</a>
+    echo '<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+      <ul class="navbar-nav">
+        <li class="nav-item active">
+          <a class="nav-link" href="https://guarded-anchorage-18964.herokuapp.com/home.php">Home</a>
         </li>
-        <li class='nav-item'>
-          <a class='nav-link' href='https://guarded-anchorage-18964.herokuapp.com/assignments.php'>Assignments</a>
+        <li class="nav-item">
+          <a class="nav-link" href="https://guarded-anchorage-18964.herokuapp.com/assignments.php">Assignments</a>
       </ul>
 
-    </nav>";
+    </nav>';
 
  ?>
