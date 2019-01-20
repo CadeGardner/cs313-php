d83930950ad338c79d6d45980fe779afd07b4bef
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-19 23:50:10 -0700

    tag change

--- web/home.php
+++ web/home.php
@@ -54,7 +54,7 @@
 
          <div class="row">
            <div class="embed-responsive embed-responsive-16by9">
-  <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></iframe>
+  <video class="embed-responsive-item" src="https://www.youtube.com/embed/zpOULjyy-n8?rel=0" allowfullscreen></video>
 </div>
          </div>
 
