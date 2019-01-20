5d4dd6188cd1ee5363adb3909ba4e2f938a17443
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-20 00:09:31 -0700

    managed active

--- web/navbar.php
+++ web/navbar.php
@@ -1,10 +1,10 @@
 
     <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
       <ul class="navbar-nav">
-        <li class="nav-item active">
+        <li class="nav-item ">
           <a class="nav-link" href="https://guarded-anchorage-18964.herokuapp.com/home.php">Home</a>
         </li>
-        <li class="nav-item">
+        <li class="nav-item active">
           <a class="nav-link" href="https://guarded-anchorage-18964.herokuapp.com/assignments.php">Assignments</a>
       </ul>
 
