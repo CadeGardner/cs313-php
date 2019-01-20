4d2ad437c2027f6ae3d43d0cf08dc5d0c32b0261
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-19 23:53:16 -0700

    Added Link

--- web/home.php
+++ web/home.php
@@ -24,10 +24,10 @@
        <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
          <ul class="navbar-nav">
            <li class="nav-item active">
-             <a class="nav-link" href="#">Home</a>
+             <a class="nav-link" href="https://guarded-anchorage-18964.herokuapp.com/home.php">Home</a>
            </li>
            <li class="nav-item">
-             <a class="nav-link" href="#">Assignments</a>
+             <a class="nav-link" href="https://guarded-anchorage-18964.herokuapp.com/assignments.php">Assignments</a>
          </ul>
 
        </nav>
