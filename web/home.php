5ee3fd87ec428f92778dfd2922e588b326c5e07d
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-19 23:43:41 -0700

    Videol Embed

--- web/home.php
+++ web/home.php
@@ -48,6 +48,10 @@
              soccer ball. A YouTube channel called "The F2"has really inspired me to <br>
              get better at the <span class="font-italic">Beautiful</span> game.
              </p>
+
+             <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.youtube.com/watch?v=nPQYvGvJ6uo" allowfullscreen></iframe>
+              </div>
          </div>
 
        </div>
