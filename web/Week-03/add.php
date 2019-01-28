3890f9deaff827421fd184f710db75305651e53f
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 16:19:00 -0700

    trying to figure out storage of more than one item

--- web/Week-03/add.php
+++ web/Week-03/add.php
@@ -1,16 +1,15 @@
 <?php
 session_start();
 
-
-
 if(isset($_POST['name'])) {
 
   $_SESSION['src'] = $_POST['source'];
   $_SESSION['name'] = $_POST['name'];
   $_SESSION['price'] = $_POST['price'];
   echo count($_SESSION['name']);
+  print_r($_SESSION);
   exit();
 }
 
-print_r($_SESSION);
+
 ?>
