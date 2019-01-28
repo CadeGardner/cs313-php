189d69e497a5fe9e4b1d5a8ab50bb8ee6ae6f3ed
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 16:25:36 -0700

    got everything storing

--- web/Week-03/add.php
+++ web/Week-03/add.php
@@ -6,10 +6,9 @@ if(isset($_POST['name'])) {
   $_SESSION['src'][] = $_POST['source'];
   $_SESSION['name'][] = $_POST['name'];
   $_SESSION['price'][] = $_POST['price'];
-  echo count($_SESSION['name']);
-  print_r($_SESSION);
+  // print_r($_SESSION);
   exit();
 }
 
-
+  echo count($_SESSION['name']);
 ?>
