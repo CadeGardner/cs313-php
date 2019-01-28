9f5b0c1eccaf9fc2ce0115ac147106d54b9056e4
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 16:22:29 -0700

    added array

--- web/Week-03/add.php
+++ web/Week-03/add.php
@@ -3,9 +3,9 @@ session_start();
 
 if(isset($_POST['name'])) {
 
-  $_SESSION['src'] = $_POST['source'];
-  $_SESSION['name'] = $_POST['name'];
-  $_SESSION['price'] = $_POST['price'];
+  $_SESSION['src'][] = $_POST['source'];
+  $_SESSION['name'][] = $_POST['name'];
+  $_SESSION['price'][] = $_POST['price'];
   echo count($_SESSION['name']);
   print_r($_SESSION);
   exit();
