1f3c1f16ca76358ed41d315bc00d2f1e413346fc
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 15:45:52 -0700

    trying to access session

--- web/Week-03/add.php
+++ web/Week-03/add.php
@@ -9,11 +9,10 @@ if(isset($_POST['source'])) {
   $_SESSION['name'] = $_POST['name'];
   $_SESSION['price'] = $_POST['price'];
   echo count($_SESSION['name']);
-exit();
 }
 
-$img = $_POST['source'];
-echo "$img";
+$img = $_POST['name'];
+echo $img;
 echo "<h1>".count($_SESSION['name'])."<h1>";
 echo "testing";
 ?>
