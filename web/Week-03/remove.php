e85ab58152d5162f1b5326abbf44af40cb9adb4c
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 18:43:31 -0700

    need to fix index

--- web/Week-03/remove.php
+++ web/Week-03/remove.php
@@ -2,11 +2,25 @@
 
 $index = $_POST['index'];
 
+for($i = 0; $i < $index; $i++;) {
+  $source[$i] = $_SESSION['src'][$i];
+  $name[$i] = $_SESSION['name'][$i];
+  $price[$i] = $_SESSION['price'][$i];
+}
+
 unset($_SESSION['src'][$index]);
 unset($_SESSION['name'][$index]);
 unset($_SESSION['price'][$index]);
 
-echo print_r($_SESSION); 
+for($i = 0; $i < count($_SESSION['src']); $i++;) {
+  $source[$i] = $_SESSION['src'][$i];
+  $name[$i] = $_SESSION['name'][$i];
+  $price[$i] = $_SESSION['price'][$i];
+}
+
+print_r(array_values($source));
+print_r(array_values($name));
+echo print_r($_SESSION);
 
 
 ?>
