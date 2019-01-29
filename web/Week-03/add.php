6911520f0ee68d7e88fc4e019fd59cd6fe7724ab
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 21:02:18 -0700

    fix

--- web/Week-03/add.php
+++ web/Week-03/add.php
@@ -7,7 +7,7 @@ if(isset($_POST['name'])) {
   $_SESSION['name'][] = $_POST['name'];
   $_SESSION['price'][] = $_POST['price'];
   // print_r($_SESSION);
-  exit();
+  //exit();
 }
   $numItems = count($_SESSION['name']);
   echo "<h3> Your Items: $numItems";
