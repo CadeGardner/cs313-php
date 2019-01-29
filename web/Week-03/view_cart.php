59be8f056f8935e31fe57543571a500b7bea8938
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 23:27:50 -0700

    style

--- web/Week-03/view_cart.php
+++ web/Week-03/view_cart.php
@@ -48,9 +48,9 @@
     <?php
       echo"<div class='row' class='cartItems'>";
       for($i = 0; $i < count($_SESSION['src']); $i++){
-      echo"<img src='".$_SESSION['src'][$i]."'>";
-      echo "<p class='row'>".$_SESSION['name'][$i]."</p>";
-      echo "<p class='row'>".$_SESSION['price'][$i]."</p>";
+      echo"<img src='".$_SESSION['src'][$i]."'><br>";
+      echo "<p>".$_SESSION['name'][$i]."</p><br>";
+      echo "<p class='row'>".$_SESSION['price'][$i]."</p><br>";
       echo " <button type='button' class='button".$i."' onclick='remove($i)'>
       Remove Item
       </button></div>";
