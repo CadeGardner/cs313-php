a1456e8fafb4cf91442385e3adac15490565931b
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 19:30:00 -0700

    another tweak

--- web/Week-03/remove.php
+++ web/Week-03/remove.php
@@ -8,18 +8,22 @@ $index = $_POST['index'];
 //   $price[$i] = $_SESSION['price'][$i];
 // }
 
-if($index == 0) {
-  array_shift($_SESSION['src'][]);
-  array_shift($_SESSION['name'][]);
-  array_shift($_SESSION['price'][]);
+$src = $_SESSION['src'];
 
-}
+// if($index == 0) {
+//   //array_shift($_SESSION['src']);
+//   array_shift($_SESSION['name']);
+//   array_shift($_SESSION['price']);
+//
+// }
 
-if($index != 0) {
+
 unset($_SESSION['src'][$index]);
 unset($_SESSION['name'][$index]);
 unset($_SESSION['price'][$index]);
-}
+
+$_SESSION['src'] = $src;
+
 //
 // for($i = 0; $i < count($_SESSION['src']); $i++) {
 //   $source[$i] = $_SESSION['src'][$i];
@@ -32,22 +36,21 @@ unset($_SESSION['price'][$index]);
 // }
 // print_r(array_values($source));
 // print_r(array_values($name));
-//echo print_r($_SESSION);
+echo print_r($_SESSION);
 
-echo"<div class='cartContainer' id='cartItems'>";
-for($i = 0; $i < count($_SESSION['src']); $i++){
-  if(!isset($_SESSION['src'][$i])) {
-    continue;
- }
- else
- {
-   echo"<img src='".$_SESSION['src'][$i]."'>";
-   echo "<p>".$_SESSION['name'][$i]."</p>";
-   echo "<p>".$_SESSION['price'][$i]."</p>";
-   echo " <button type='button' name='button' onclick='remove($i)'>
-  Remove Item
-</button>";
- }
-}
+// for($i = 0; $i < count($_SESSION['src']); $i++){
+//   if(!isset($_SESSION['src'][$i])) {
+//     continue;
+//  }
+//  else
+//  {
+//    echo"<img src='".$_SESSION['src'][$i]."'>";
+//    echo "<p>".$_SESSION['name'][$i]."</p>";
+//    echo "<p>".$_SESSION['price'][$i]."</p>";
+//    echo " <button type='button' name='button' onclick='remove($i)'>
+//   Remove Item
+// </button>";
+//  }
+// }
 
 ?>
