e7795a024ecd0a489376ac88464c273d95518ff5
Cade Gardner <cadegardner@Cades-MacBook-Pro.local>
2019-01-28 18:55:49 -0700

    testing print loop

--- web/Week-03/remove.php
+++ web/Week-03/remove.php
@@ -17,12 +17,21 @@ for($i = 0; $i < count($_SESSION['src']); $i++) {
   $name[$i] = $_SESSION['name'][$i];
   $price[$i] = $_SESSION['price'][$i];
 }
-foreach ($_SESSION['src'] as $value) {
-
-}
-print_r(array_values($source));
-print_r(array_values($name));
+// foreach ($_SESSION['src'] as $value) {
+//
+// }
+// print_r(array_values($source));
+// print_r(array_values($name));
 //echo print_r($_SESSION);
 
+echo"<div class='cartContainer' id='cartItems'>";
+for($i = 0; $i < count($_SESSION['src']); $i++){
+echo"<img src='".$_SESSION['src'][$i]."'>";
+echo "<p>".$_SESSION['name'][$i]."</p>";
+echo "<p>".$_SESSION['price'][$i]."</p>";
+echo " <button type='button' name='button' onclick='remove($i)'>
+   Remove Item
+ </button>";
+}
 
 ?>
