f037b0fe902b0b4bc4b3597d13f0df0d25139bb9
Cade Gardner <gar17040@byui.edu>
2019-02-07 10:11:35 -0700

    Team Activity Activate

---
+++ web/Team_05/team_05.php
@@ -0,0 +1,38 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Read Yo Scriptures!</title>
+  </head>
+  <body>
+    <h1>Scriptures Resources</h1>
+    <?php
+
+      try
+      {
+      $dbUrl = getenv('DATABASE_URL');
+      $dbOpts = parse_url($dbUrl);
+      $book = $dbOpts["host"];
+      $dbPort = $dbOpts["port"];
+      $dbUser = $dbOpts["user"];
+      $dbPassword = $dbOpts["pass"];
+      $dbName = ltrim($dbOpts["path"],'/');
+      $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
+      $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      }
+      catch (PDOException $ex)
+      {
+      echo 'Error!: ' . $ex->getMessage();
+      die();
+      }
+
+      $scripture = $db->query('SELECT * FROM scriptures');
+      while($row = $scripture->fetch(PDO::FETCH_ASSOC))
+      {
+        echo "<span style='font-weight: bold'>".$row['book'].$row['chapter'].':'.
+        $row['verse']."</span>".' - '.$row['content'].'<br>';
+      }
+     ?>
+  </body>
+</html>
