7a13bdcc2fb5a213b0b854036609d52fa9ff0a13
Cade Gardner <gar17040@byui.edu>
2019-02-12 20:25:19 -0700

    query change

--- web/Photo_Project/appt_display.php
+++ web/Photo_Project/appt_display.php
@@ -2,14 +2,19 @@
   require "db_connect.php";
   $db = get_db();
 
-$appointment = $db->prepare("SELECT appointment, user_id,
-  calendar_id, package_id, message_id FROM appointment");
+$appointment = $db->prepare("SELECT * FROM appointment a
+JOIN client c ON c.id = a.user_id
+JOIN calendar cal ON cal.id = a.calendar_id
+JOIN package p ON p.id = a.package_id");
+
+
+// SELECT appointment, user_id,
+// calendar_id, package_id, message_id FROM appointment
 $appointment->execute();
 // Go through each result
 echo "Testing";
 while ($row = $appointment->fetch(PDO::FETCH_ASSOC))
 {
-  echo"looping";
 	// The variable "row" now holds the complete record for that
 	// row, and we can access the different values based on their
 	// name
