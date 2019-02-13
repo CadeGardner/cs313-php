a2ddee8b1ff463af586a9b186a490d2579ad3760
Cade Gardner <gar17040@byui.edu>
2019-02-12 20:36:23 -0700

    another query fix

--- web/Photo_Project/appt_display.php
+++ web/Photo_Project/appt_display.php
@@ -2,10 +2,11 @@
   require "db_connect.php";
   $db = get_db();
 
-$appointment = $db->prepare("SELECT * FROM appointment a
-JOIN client c ON 'c.id = a.user_id'
-JOIN calendar cal ON 'cal.id = a.calendar_id'
-JOIN package p ON 'p.id = a.package_id'");
+$appointment = $db->prepare("SELECT appointment, name, appt ,description
+FROM appointment a
+JOIN client c ON c.id = a.user_id
+JOIN calendar cal ON cal.id = a.calendar_id
+JOIN package p ON p.id = a.package_id");
 
 
 // SELECT appointment, user_id,
@@ -18,10 +19,10 @@ while ($row = $appointment->fetch(PDO::FETCH_ASSOC))
 	// The variable "row" now holds the complete record for that
 	// row, and we can access the different values based on their
 	// name
-	$d_username = $row['user_id'];
+	$d_username = $row['name'];
 	$d_time = $row['appointment'];
-	$d_date = $row['calendar_id'];
-	$d_package = $row['package_id'];
+	$d_date = $row['appt'];
+	$d_package = $row['description'];
 	echo "<div>$d_username<br> $d_time<br> $d_date<br>$d_package</div>";
 }
  ?>
