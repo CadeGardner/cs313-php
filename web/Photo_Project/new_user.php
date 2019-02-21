1f4f79e990b19939b1ee000058cda7d350b27a43
Cade Gardner <gar17040@byui.edu>
2019-02-21 16:45:31 -0700

    removed parameter

--- web/Photo_Project/new_user.php
+++ web/Photo_Project/new_user.php
@@ -21,7 +21,7 @@ $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
 
 $db = get_db();
 echo "$db";
-$query = 'INSERT INTO client(name, password, user) VALUES(:client_name, :password)';
+$query = 'INSERT INTO client(name, password) VALUES(:client_name, :password)';
 $statement = $db->prepare($query);
 $statement->bindValue(':client_name', $client_name);
 $statement->bindValue(':password', $hashedPassword);
