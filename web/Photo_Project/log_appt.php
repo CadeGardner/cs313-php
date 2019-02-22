4d9ec1ec034219d701d6996e51363a15b8f911e4
Cade Gardner <gar17040@byui.edu>
2019-02-21 17:49:37 -0700

    Package id test

--- web/Photo_Project/log_appt.php
+++ web/Photo_Project/log_appt.php
@@ -14,26 +14,32 @@ require("db_connect.php");
 $db = get_db();
 
 $client_query = 'SELECT id FROM client WHERE name = :client_name';
-$user_query = $db->prepare($client_query);
-$user_query->bindValue(':client_name', $username);
-$user_query->execute();
+$user_statement = $db->prepare($client_query);
+$user_statement->bindValue(':client_name', $username);
+$user_statement->execute();
 
-$user_row = $user_query->fetch(PDO::FETCH_ASSOC);
+$user_row = $user_statement->fetch(PDO::FETCH_ASSOC);
 $user_id = $user_row['id'];
 
 echo "User id is: $user_id <br>";
 
 $calendar_query = 'SELECT id FROM calendar WHERE  appt = :appt';
-$date_query = $db->prepare($calendar_query);
-$date_query->bindValue(':appt', $appt);
-$date_query->execute();
-$date_row = $date_query->fetch(PDO::FETCH_ASSOC);
+$date_statement = $db->prepare($calendar_query);
+$date_statement->bindValue(':appt', $appt);
+$date_statement->execute();
+$date_row = $date_statement->fetch(PDO::FETCH_ASSOC);
 $calendar_id = $date_row['id'];
 
 echo "Date id is: $calendar_id";
 
 
-// $package_query = 'SELECT id FROM package WHERE  package = :package';
+$package_query = 'SELECT id FROM package WHERE  package = :package';
+$package_statement = $db->prepare($package_query);
+$package_statement->bindValue(':package', $package);
+$package_statement->execute();
+$package_row = $package_statement->fetch(PDO::FETCH_ASSOC);
+$package_id = $package_row['id'];
+
 // $time_query = 'INSERT INTO appointment(time, user_id, calendar_id, package_id, message_id)';
 //
 // $query = 'INSERT INTO client(name, password) VALUES(:client_name, :password)';
