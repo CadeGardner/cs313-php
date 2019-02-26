6f7b459c118e66f5f5dde5a369bae59c96511603
Cade Gardner <gar17040@byui.edu>
2019-02-26 06:38:37 -0700

    bootstrap

--- web/Photo_Project/home.php
+++ web/Photo_Project/home.php
@@ -3,7 +3,20 @@
   <head>
     <meta charset="utf-8">
     <meta name="" content="">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+  <style>
+  /* Make the image fully responsive */
+  .carousel-inner img {
+      width: 100%;
+      height: 100%;
+  }
+  </style>
     <title>Elaine Lewis Photography</title>
+
+
   </head>
   <body>
     <h1>E Lewis Photography</h1>
