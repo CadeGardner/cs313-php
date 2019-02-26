d2ca81d43a38af575d4ff3ec2b19f1efadf2f452
Cade Gardner <gar17040@byui.edu>
2019-02-26 06:37:33 -0700

    buttons

--- web/Photo_Project/home.php
+++ web/Photo_Project/home.php
@@ -59,6 +59,13 @@
 
       </div>
 
+      <a class="carousel-control-prev" href="#demo" data-slide="prev">
+        <span class="carousel-control-prev-icon"></span>
+      </a>
+      <a class="carousel-control-next" href="#demo" data-slide="next">
+        <span class="carousel-control-next-icon"></span>
+      </a>
+
     </div>
 
 
