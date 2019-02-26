d9fe945a82f9671575284d611d11fc62eaff4df4
Cade Gardner <gar17040@byui.edu>
2019-02-26 09:09:34 -0700

    css

--- web/Photo_Project/register.php
+++ web/Photo_Project/register.php
@@ -10,9 +10,7 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     <style media="screen">
-    	button{
-    		background-color: #3c6e71; /*teal*/
-    	}
+
 
       .jumbotron-fluid{
         background-color: #3c6e71; /*teal*/
