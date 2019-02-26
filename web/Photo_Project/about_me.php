967c362287ee16e48265d5e3d8dcb8267c4e5390
Cade Gardner <gar17040@byui.edu>
2019-02-26 08:24:52 -0700

    bootstrap

--- web/Photo_Project/about_me.php
+++ web/Photo_Project/about_me.php
@@ -2,7 +2,16 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
+    <meta name="viewport" content="width = device-width, initial-scale = 1" />
     <title>Getting to Know Me</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <link href="main.css" type="text/css" rel="stylesheet" />
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+
   </head>
   <body>
     <div class="container">
