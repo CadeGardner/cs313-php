52d495036309b25eea191a6f5a50a50e354dbf90
Cade Gardner <gar17040@byui.edu>
2019-02-26 05:37:35 -0700

    another test

--- web/Photo_Project/appt_display.php
+++ web/Photo_Project/appt_display.php
@@ -1,51 +1,54 @@
 <?php
   require "db_connect.php";
   $db = get_db();
+  require 'build_calendar.php';
+  $calendar = new Calendar;
+  echo $calendar->show();
+  $num_days = $calendar->getTotalDays();
+  $num_month = $calendar->getCurrentMonth();
 
-$appointment = $db->prepare("SELECT appointment, name, appt, package
-FROM appointment a
-JOIN client c ON c.id = a.user_id
-JOIN calendar cal ON cal.id = a.calendar_id
-JOIN package p ON p.id = a.package_id");
 
-// SELECT appointment, user_id,
-// calendar_id, package_id, message_id FROM appointment
-$appointment->execute();
-// Go through each result
-while ($row = $appointment->fetch(PDO::FETCH_ASSOC))
-{
-	// The variable "row" now holds the complete record for that
-	// row, and we can access the different values based on their
-	// name
-	$d_username = $row['name'];
-	$d_time = $row['appointment'];
-	$d_date = $row['appt'];
-	$d_package = $row['package'];
-	echo "<div>$d_username<br> $d_time<br> $d_date<br>$d_package</div><br>";
+  $appointment = $db->prepare("SELECT appointment, name, appt, package
+  FROM appointment a
+  JOIN client c ON c.id = a.user_id
+  JOIN calendar cal ON cal.id = a.calendar_id
+  JOIN package p ON p.id = a.package_id
+  WHERE EXTRACT(MONTH FROM appt) = $num_month");
 
-}
+  // SELECT appointment, user_id,
+  // calendar_id, package_id, message_id FROM appointment
+  $appointment->execute();
+  // Go through each result
+  while ($row = $appointment->fetch(PDO::FETCH_ASSOC))
+  {
+  	// The variable "row" now holds the complete record for that
+  	// row, and we can access the different values based on their
+  	// name
+  	$d_username = $row['name'];
+  	$d_time = $row['appointment'];
+  	$d_date = $row['appt'];
+  	$d_package = $row['package'];
+  	echo "<div>$d_username<br> $d_time<br> $d_date<br>$d_package</div><br>";
 
-require 'build_calendar.php';
-$calendar = new Calendar;
-echo $calendar->show();
-$num_days = $calendar->getTotalDays();
-$num_month = $calendar->getCurrentMonth();
-echo "<br>Total Days: $num_days <br> Current Month: $num_month";
+  }
 
-$month_query = "SELECT appointment, appt FROM appointment a
-JOIN calendar cal ON cal.id = a.calendar_id
-WHERE EXTRACT(MONTH FROM appt) = $num_month";
-$num_month = $db->prepare($month_query);
-$num_month->bindValue(':num_month', $num_month);
-$num_month->execute();
 
-while ($arow = $num_month->fetch(PDO::FETCH_ASSOC))
-{
-  echo "loop entered";
-  $date_array = date_parse($arow['appt']);
-  $current_appt = $date_array['day'];
-  echo "$current_appt";
-}
+  echo "<br>Total Days: $num_days <br> Current Month: $num_month";
+
+  $month_query = "SELECT appointment, appt FROM appointment a
+  JOIN calendar cal ON cal.id = a.calendar_id
+  WHERE EXTRACT(MONTH FROM appt) = $num_month";
+  $num_month = $db->prepare($month_query);
+  $num_month->bindValue(':num_month', $num_month);
+  $num_month->execute();
+
+  while ($month_row = $num_month->fetch(PDO::FETCH_ASSOC))
+  {
+    echo "loop entered";
+    $date_array = date_parse($month_row['appt']);
+    $current_appt = $date_array['day'];
+    echo "$current_appt";
+  }
 
 ?>
 <!DOCTYPE html>
