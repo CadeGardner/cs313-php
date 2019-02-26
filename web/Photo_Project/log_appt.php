179093dde4d1928c61e545e2388c7edb7eb089f5
Cade Gardner <gar17040@byui.edu>
2019-02-26 00:30:07 -0700

    reactivation of appt insert

--- web/Photo_Project/log_appt.php
+++ web/Photo_Project/log_appt.php
@@ -45,19 +45,19 @@ $package_id = $package_row['id'];
 // echo "Package id is: $package_id";
 $message_id = 1;
 // echo "$time";
-// $time_query = 'INSERT INTO appointment (appointment, user_id, calendar_id, package_id, message_id)
-// VALUES (:appointment, :user_id, :calendar_id, :package_id, :message_id)';
-//
-// $statement = $db->prepare($time_query);
-// $statement->bindValue(':user_id', $user_id);
-// $statement->bindValue(':calendar_id', $calendar_id);
-// $statement->bindValue(':appointment', $time);
-// $statement->bindValue(':package_id', $package_id);
-// $statement->bindValue(':message_id', $message_id);
-// $statement->execute();
-//
-// flush();
-// header("Location: appt_display.php");
-// die();
+$time_query = 'INSERT INTO appointment (appointment, user_id, calendar_id, package_id, message_id)
+VALUES (:appointment, :user_id, :calendar_id, :package_id, :message_id)';
+
+$statement = $db->prepare($time_query);
+$statement->bindValue(':user_id', $user_id);
+$statement->bindValue(':calendar_id', $calendar_id);
+$statement->bindValue(':appointment', $time);
+$statement->bindValue(':package_id', $package_id);
+$statement->bindValue(':message_id', $message_id);
+$statement->execute();
+
+flush();
+header("Location: appt_display.php");
+die();
 
  ?>
