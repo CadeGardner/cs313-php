74f7407049f6b2e7c459c877a0efb28e87ece0bf
Cade Gardner <gar17040@byui.edu>
2019-02-26 08:26:19 -0700

    css

--- web/Photo_Project/about_me.php
+++ web/Photo_Project/about_me.php
@@ -10,7 +10,11 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
 
-
+    <style media="screen">
+      .jumbotron{
+        background-color: background-color: #3c6e71; /*teal*/
+      }
+    </style>
 
   </head>
   <body>
