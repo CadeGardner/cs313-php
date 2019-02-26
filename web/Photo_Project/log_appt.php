5da411ac104e6cde309c0b54c258bfd0d49689d8
Cade Gardner <gar17040@byui.edu>
2019-02-26 00:44:24 -0700

    small change for date addition

--- web/Photo_Project/log_appt.php
+++ web/Photo_Project/log_appt.php
@@ -36,9 +36,14 @@ if(!isset($date_statement))
   $date_statement = $db->prepare($calendar_insert_query);
   $date_statement->bindValue(':appt', $appt);
   $date_statement->execute();
+  $date_row = $date_statement->fetch(PDO::FETCH_ASSOC);
+  $calendar_id = $date_row['id'];
 }
-$date_row = $date_statement->fetch(PDO::FETCH_ASSOC);
-$calendar_id = $date_row['id'];
+else {
+  $date_row = $date_statement->fetch(PDO::FETCH_ASSOC);
+  $calendar_id = $date_row['id'];
+}
+
 
 //echo "Date id is: $calendar_id <br>";
 
