05fcdbfb01ba0d2ea58f9d15a5ee881225eff7d1
Cade Gardner <gar17040@byui.edu>
2019-02-26 06:35:30 -0700

    carousel

--- web/Photo_Project/home.php
+++ web/Photo_Project/home.php
@@ -22,11 +22,39 @@
       <div class="carousel-inner">
 
         <div class="carousel-item active">
-            <img src="" alt="">
+            <img src="family.jpg" alt="photo of family" />
         </div>
 
         <div class="carousel-item">
-          <img src="" alt="">
+          <img src="bridesmaids.jpg" alt="bridesmaids jumping" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="child1.jpg" alt="girl posing" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="jumping.jpg" alt="family posing" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="seniorboy.jpg" alt="boy graduation" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="weddingdip.jpg" alt="couple kiss" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="tigers.jpg" alt="cross country team" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="seniorgirl.jpg" alt="girl graduation" />
+        </div>
+
+        <div class="carousel-item">
+          <img src="girlportrait.jpg" alt="girl portrait" />
         </div>
 
       </div>
